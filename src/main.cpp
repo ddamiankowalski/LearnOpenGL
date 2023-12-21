9b1281bbc46d7c2dcd5061709247d7a6a32d486f
Damian Kowalski <dkowalski.1997@gmail.com>
2023-12-21 14:09:48 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include "glad.h"
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void windowResizeCallback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void static InitializeGlfw()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+GLFWwindow* CreateWindow()
+{
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    return window;
+}
+
+int main()
+{
+    InitializeGlfw();
+    GLFWwindow* window = CreateWindow();
+
+    if(window == NULL)
+    {
+        std::cout << "Failed to create glfw window" << std::endl;
+        glfwTerminate();
+    }
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }  
+
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, windowResizeCallback);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
