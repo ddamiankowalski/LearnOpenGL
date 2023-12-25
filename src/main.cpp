9eccf3badb07e3d1bfe6c0624b53dbccfd150f16
Damian Kowalski <dkowalski.1997@gmail.com>
2023-12-25 13:44:57 +0100

    working view rotation

--- src/main.cpp
+++ src/main.cpp
@@ -60,13 +60,11 @@
         -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
-// float vertices[] = {
-//      // positions          // colors           // texture coords
-//      0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 0.0f, // top right
-//      0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 1.0f, // bottom right
-//     -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 1.0f, // bottom left
-//     -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 0.0f  // top left 
-// };
+glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f, 0.0f, 0.0f),
+    glm::vec3( 2.0f, 5.0f, -15.0f),
+    glm::vec3( -1.0f, -5.0f, -2.5f),
+};
 
 unsigned int indices[] = {
     0, 2, 3,
@@ -181,7 +179,7 @@ int main()
     glUniform1i(glGetUniformLocation(triangleShader.ID, "ourTexture2"), 1);
 
     glm::mat4 model = glm::mat4(1.0f);
-    model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+    model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.0f, 0.0f, 0.0f));
     
     glm::mat4 view = glm::mat4(1.0f);
     view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
@@ -189,14 +187,13 @@ int main()
     glm::mat4 projection;
     projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
+    glEnable(GL_DEPTH_TEST);
+
     while(!glfwWindowShouldClose(window))
     {
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glm::mat4 model = glm::mat4(1.0f); 
-        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-        int modelLoc = glGetUniformLocation(triangleShader.ID, "model");
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        view = glm::rotate(view, glm::radians((float)glfwGetTime() / 10), glm::vec3(1.0f, 0.0f, 0.0f));
 
         int viewLoc = glGetUniformLocation(triangleShader.ID, "view");
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
@@ -207,7 +204,19 @@ int main()
         processInput(window);
         triangleShader.use();
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        
+        for(unsigned int i = 0; i < 3; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f); 
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f;
+            model = glm::rotate(model, glm::radians((float)glfwGetTime() * 100), glm::vec3(1.0f, 0.0f, 0.0f));
+
+            int modelLoc = glGetUniformLocation(triangleShader.ID, "model");
+            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
